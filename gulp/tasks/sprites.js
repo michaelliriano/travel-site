8f03e113361a2d90d40564a55b83943bb87efe12
Michael <mikethecoder96@gmail.com>
2019-07-30 22:33:22 -0400

    Footer

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,30 +1,38 @@
 var gulp = require('gulp'),
-svgSprite = require('gulp-svg-sprite'),
-rename =require('gulp-rename');
-
-
+    svgSprite = require('gulp-svg-sprite'),
+    rename = require('gulp-rename');
+ 
 var config = {
-    mode: {
+  mode: {
+    css: {
+      sprite: 'sprite.svg',
+      render: {
         css: {
-            render: {
-                css: {
-                    template: './gulp/template/sprite.css'
-                }
-            }
+          template: './gulp/templates/sprite.css'
         }
+      }
     }
+  }
 }
-
-gulp.task('createSprite', function() {
-    return gulp.src('./app/assets/images/icons/**/*.svg')
-        .pipe(svgSprite(config))
-        .pipe(gulp.dest('./app/temp/sprite/'));
-});
-
-gulp.task('copySpriteCSS', ['createSprite'], function() {
-    return gulp.src('./app/temp/sprite/css/*.css')
-        .pipe(rename('_sprite.css'))
-        .pipe(gulp.dest('./app/assets/styles/modules'));
-});
-
-gulp.task('icons', gulp.series('createSprite', 'copySpriteCSS'));
+ 
+gulp.task('createSprite', createSprite);
+function createSprite() {
+  return gulp.src('./app/assets/images/icons/**/*.svg')
+    .pipe(svgSprite(config))
+    .pipe(gulp.dest('./app/temp/sprite/'));
+};
+ 
+gulp.task('copySpriteGraphic', copySpriteGraphic);
+function copySpriteGraphic() {
+  return gulp.src('./app/temp/sprite/css/**/*.svg')
+    .pipe(gulp.dest('./app/assets/images/sprites'));
+};
+ 
+gulp.task('copySpriteCSS', copySpriteCSS);
+function copySpriteCSS() {
+  return gulp.src('./app/temp/sprite/css/*.css')
+    .pipe(rename('_sprite.css'))
+    .pipe(gulp.dest('./app/assets/styles/modules'));
+};
+ 
+gulp.task('icons', gulp.series('createSprite', gulp.parallel('copySpriteGraphic', 'copySpriteCSS')));
